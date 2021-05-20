67737875cc70ff42262d98f4bce75f735ab8f74f
Jóse Gregory <divisanaranja10@gmail.com>
2021-05-20 07:52:49 -0500

    agregar code

---
+++ Taller01/pitstopGregoryNieto.py
@@ -0,0 +1,17 @@
+message="EXERCISE PIT STOP"
+print(message)
+car_stop="Se detiene el vehículo y al instante y todos al tiempo realizan:"
+print(car_stop)
+hombre_1="Asistente 1 cambia llanta 1"
+print(hombre_1)
+hombre_2="Asistente 2 cambia llanta 2"
+print(hombre_2)
+hombre_3="Asistente 3 cambia llanta 3"
+print(hombre_3)
+hombre_4="Asistente 4 cambia llanta 4"
+print(hombre_4)
+hombre_5="Asistente 5 tanquea el vehículo"
+print(hombre_5)
+car_run="Todo listo en 3 sg y arranca el vehículo"
+print(car_run)
+
