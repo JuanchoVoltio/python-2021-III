648c4a7fffc1376f7f625be6c4d8ab0514a8b641
Diaz <diazcallejuangabriel13@gmail.com>
2021-05-20 07:59:04 -0500

    practica1

---
+++ Taller01/taller01_juan.py
@@ -0,0 +1,13 @@
+print("========================")
+print("taller01, parada de pits")
+print("========================"\n)
+
+cambiar_llanta1 = "cambio de llanta 1, 'Si' \n"
+cambiar_llanta2 = "cambio de llanta 2, 'Si' \n"   
+tanquear_gasolina = "tarquear gasolina,'No' \n"
+
+print(cambiar_llanta1)
+print(cambiar_llanta2)
+print(tanquear_gasolina)
+print("Fin.")
+
