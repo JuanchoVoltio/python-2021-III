38ee00eb6c2893dce75ec06d859f6a5c60f6155c
Diaz <diazcallejuangabriel13@gmail.com>
2021-05-21 10:21:32 -0500

    practica1

--- Taller01/taller01_juan.py
+++ Taller01/taller01_juan.py
@@ -1,3 +1,5 @@
+import random
+
 print("========================")
 print("taller01, parada de pits")
 print("========================\n")
@@ -5,16 +7,16 @@ print("========================\n")
 vehiculoInactivo = "llega el vehículo a pits, sincrónicamente dos mecánicos por rueda cambian: \n"
 
 mecanicos_1_2 = "cambian llanta 1, 'Si', duración cambio de llanta en segundos: \n"
-import random
+
 random_variable_1_2 = random.randint(1,3)
 mecanicos_3_4 = "cambian llanta 2, 'Si', duración cambio de llanta en segundos: \n"
-import random
+
 random_variable_3_4 = random.randint(1,3)
 mecanicos_5_6 = "cambian llanta 3, 'Si', duración cambio de llanta en segundos: \n"
-import random
+
 random_variable_5_6 = random.randint(1,3)
 mecanicos_7_8 = "cambian llanta 4, 'Si', duración cambio de llanta en segundos: \n"
-import random
+
 random_variable_7_8 = random.randint(1,3)
 tanquear = "repostaje de combustible,'Sin Tanqueo' \n"
 vehiculoActivo = "Sincronización completa, estado OK, salida según tiempo promedio gastado por cambio de llanta en segundos \n"
