dc3afabe1dccc90623afddaaf0bb8d4e879d76ce
Henry L. Gómez <henryl.gomez@gmail.com>
2021-05-27 08:25:53 -0500

    Agregar ejemplo diccionarios y funciones

--- Ejemplos/ejemplo_dictionary.py
+++ Ejemplos/ejemplo_dictionary.py
@@ -1,5 +1,21 @@
 
-my_dict = {'a':10, 'b':30, 'c':50, 3:40, -2:20}
-my_other_dict = dict('a'=10, 'd'=90)
+my_dict = {
+    'a' : 10,
+    'b' : 30,
+    'c' : 50,
+    3 : 40,
+    -2 : 20
+    }
 
-print(my_other_dict)
+for current_key in my_dict: #Iterar sobre las llaves
+    print(current_key)
+
+for current_value in my_dict.values(): #Iterar sobre los valores
+    print(current_value)
+
+for curr_key, curr_value in my_dict.items(): #Iterar sobre las llaves y valores
+    print("key: ", curr_key, " | value: ", curr_value)
+
+my_dict.pop('c')
+
+print(my_dict)
