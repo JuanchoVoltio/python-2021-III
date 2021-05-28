09d344e50bf515144be69911172d4405e9ee87c2
Diaz <diazcallejuangabriel13@gmail.com>
2021-05-28 08:39:38 -0500

    practica

---
+++ Taller05/sum_res_mul_div_diaz.py
@@ -0,0 +1,11 @@
+def math_operation(num1, num2, operador):
+    if operator == '+':
+        print(num1 + num2)
+    elif operator == '-':
+        print(num1 - num2)
+    elif operator == '*':
+        print(num1 * num2)
+    elif operator == '/':
+        print(num1 / num2)
+    else:
+        print("Unknown operation")
