2e0091ab3e5bd0810f8b9c00a9ef3720a70212e3
Jóse Gregory <divisanaranja10@gmail.com>
2021-05-30 20:32:02 -0500

    cargar archivo

---
+++ Taller04/Niño_pregunton_GregoryNieto.py
@@ -0,0 +1,13 @@
+print('**************************************')
+print('*EXERCISE NIÑO PREGUNTÓN *')
+print('**************************************\n')
+
+boy = input("Ask a question: ")
+adult = input("Answer: ")
+
+while adult != "porque si":
+
+    boy = input("Ask a question: ") 
+    adult = input("Answer: ")
+
+print("La bendición se va a jetiar!")
