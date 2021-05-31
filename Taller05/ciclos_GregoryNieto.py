7e9ea73002c2a8ced527e1be87c852fa1858993e
Jóse Gregory <divisanaranja10@gmail.com>
2021-05-30 20:33:45 -0500

    agregar archivo

---
+++ Taller05/ciclos_GregoryNieto.py
@@ -0,0 +1,31 @@
+print('*************************')
+print('*EXERCISE CICLOS *')
+print('*************************\n')
+
+'''# Figura A
+
+h = 5
+b = 6
+
+for i in range (1, h1):
+    for i in range(1, b):
+        print("*", end="")
+    print("*")'''
+
+'''
+#Figura B
+
+h=5
+
+for i in range (h+1):
+    print("*"*i)'''
+
+#Figura C
+
+h= 4
+b= 7
+
+for i in range(h, h-1):
+    for i in range (b):
+        print("*", end="*")
+    print("*", end="*")
