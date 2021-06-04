c67bfbf12492ec25041b8dd8cd9e20a38b89d200
Jóse Gregory <divisanaranja10@gmail.com>
2021-06-04 18:39:13 -0500

    Agregar archivo

---
+++ Taller06/listas_GregoryNieto.py
@@ -0,0 +1,17 @@
+print('================')
+print('*EXERCISE LISTAS*')
+print('================')
+
+lista = [2, 'cinco', 21, 'hello', 456, [3, 5, 'dos']]
+
+print (lista)
+
+lista [2:3] = [7] # cambio el valor que esta entre la posición 1 y 2
+lista2 = lista [1]  # pregunto por el valor que esta en la posición 1, (debe imprimir la cadena 'cinco'
+
+print (lista2)
+
+
+print (lista [5][1]) # imprimo el elemento en la posicion 1 (que es el 5) de la lista dentro de la lista
+
+print (lista)
