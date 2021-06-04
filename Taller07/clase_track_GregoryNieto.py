a370eb992aa01ca59ed2527f059c10d5b94c93e5
Jóse Gregory <divisanaranja10@gmail.com>
2021-06-04 18:42:23 -0500

    agregar archivo

---
+++ Taller07/clase_track_GregoryNieto.py
@@ -0,0 +1,22 @@
+'''print('***********************************************')
+print('*EXERCISE CLASE Y FUNCIONES (track)*')
+print('***********************************************\n')'''
+
+class F1Track:
+    name = ''
+    place = ''
+    bonus = ''
+
+def __init__(self, name, place)
+    self.name = name
+    self.place = place
+
+def get_bonus (self, tipo):
+    pass
+    
+def validacion(self, x):
+        descuento = x
+        return descuento
+
+
+
